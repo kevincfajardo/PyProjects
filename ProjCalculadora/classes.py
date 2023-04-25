89030c89555eef9ad4842e77467fae8ad8413997
unknown <kevincfajardo@gmail.com>
2023-04-25 17:09:22 -0300

    calculadora funcionando, implementar eventListenner

--- ProjCalculadora/classes.py
+++ ProjCalculadora/classes.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt # type: ignore
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QLineEdit, QGridLayout # type: ignore
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QLineEdit, QGridLayout, QLabel # type: ignore
 from func import decorator, insert_txt
 
 main_style = "background-color: #140f07; color: #3dfff9;  border: 1px solid; border-radius: 5px;"
@@ -10,11 +10,13 @@ class main_window(QMainWindow):
   
         self.main_widget = QWidget()
         self.main_layout = QVBoxLayout()
+        self.operation = oper_label()
         self.field = calc_field()
-        self.grid_layout = grid_btn(self.field)
+        self.grid_layout = grid_btn(self.field, self.operation)
 
         self.setCentralWidget(self.main_widget)
         self.main_widget.setLayout(self.main_layout)
+        self.main_layout.addWidget(self.operation)
         self.main_layout.addWidget(self.field)
         self.main_layout.addLayout(self.grid_layout)
         
@@ -22,6 +24,14 @@ class main_window(QMainWindow):
         self.setWindowTitle("Calculadora")
         self.setStyleSheet("background-color: #102841;")
         
+
+class oper_label(QLabel):
+    def __init__(self, *args, **kwarg):
+        super().__init__(*args, **kwarg)
+
+        self.setStyleSheet(main_style + "font-size: 14px; border-color: #125051; color: #17a19a;")
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setContentsMargins(3, 3, 3, 3)
 
 
 class calc_field(QLineEdit):
@@ -34,32 +44,32 @@ class calc_field(QLineEdit):
         self.setMinimumWidth(450)
         self.setTextMargins(10, 10, 10, 10)
 
+        self.cont_flag = True
 
 
 class grid_btn(QGridLayout):
-    def __init__(self, field, *args, **kwarg):
+    def __init__(self, field, oper, *args, **kwarg):
         super().__init__(*args, **kwarg)
-        self.btn_maker(field)
+        self.btn_maker(field, oper)
 
     mask = [
-        ["C", "D", "x²", "/"],
-        ["7", "8", "9", "x"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["+/-", "0", ",", "E"]
+        ["Cl", "Del", "x²", "/"],
+        [7, 8, 9, "x"],
+        [4, 5, 6, "-"],
+        [1, 2, 3, "+"],
+        ["+/-", 0, ",", "E"]
     ]
 
-    def btn_maker(self, field):
+    def btn_maker(self, field, oper):
         for c, row in enumerate(self.mask):
             for i, col in enumerate(row):
                 btn_txt = f"{col}"
                 btn = QPushButton(btn_txt)
-                btn.setProperty('cssClass', 'specialButton')
 
                 if btn_txt in "0123456789":
                     btn.setStyleSheet(main_style + "height: 65; border-color: #28aeff; ")
                 else:
                     btn.setStyleSheet(main_style + "height: 65; border-color: #3dfff9; ")
                 
-                btn.clicked.connect(decorator(insert_txt, btn, field))
+                btn.clicked.connect(decorator(insert_txt, btn, field, oper))
                 self.addWidget(btn, c, i)
--- ProjCalculadora/func.py
+++ ProjCalculadora/func.py
@@ -6,20 +6,59 @@ def decorator(func, *args):
         func(*args)
     return slot
 
-def insert_txt(btn, field):
+def insert_txt(btn, field, oper):
+    global left_op
     value = btn.text()
-    leftOp = ""
 
     if value in "0123456789":
-        field.insert(btn.text())
+        if not field.cont_flag:
+            field.setText("")
+
+        field.insert(value)
+        field.cont_flag = True
 
     if value == ',':
-        field.insert(".")
+        if "." not in field.text():
+            field.insert(".")  
     
     if value in "+-x/":
-        leftOp = field.text()
-        field.setText("")
-        print(leftOp)
+        if check_value(field.text(), field):
+            field.insert(value)
+            oper.setText(field.text())
+            left_op = field.text().replace("x", "*")
+            field.setText("")
     
-    if value == "E":
-        ...
+    if value.lower() == "e":
+        try:
+            if check_value(field.text(), field):
+                operation = left_op+field.text()
+                resul = eval(operation)
+                oper.setText(operation)
+                field.setText(str(resul))
+                field.cont_flag = False
+        except (SyntaxError, NameError):
+            field.setText("Erro: operação incompleta")
+            field.cont_flag = False
+
+    if value == "Del":
+        field.backspace()
+
+    if value == "Cl":
+        field.setText("")
+        left_op = ""
+        field.cont_flag = True
+    
+    if value == "+/-":
+        if check_value(field.text(), field):
+            number = float(field.text()) * -1
+            field.setText(str(number))
+
+
+def check_value(value, field):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        field.setText("Erro: suporta apenas numeros")
+        field.cont_flag = False
+        return False
